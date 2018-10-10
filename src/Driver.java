0bda8e2564521caeb610e827fd76989e3e5c9d37
RyleyMichael <ryleymichael127@live.com>
2018-10-10 16:22:29 -0600

    initial commit

---
+++ src/Driver.java
@@ -0,0 +1,13 @@
+/**
+ * Driver class to read in the file and handle the progress through the assignment
+ *
+ * Connor Grace & Ryley Rodriguez
+ * CSCI 446 - Artificial Intelligence
+ * Fall 2018
+ */
+public class Driver {
+    public static void main (String[] args)
+    {
+        //
+    }
+}
