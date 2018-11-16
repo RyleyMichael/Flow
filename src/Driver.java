bfcc93a5fd6235a9bb5570596cbd24fccef05f0d
RyleyMichael <ryleymichael127@live.com>
2018-11-15 22:37:44 -0700

    made file names arguments

--- src/Driver.java
+++ src/Driver.java
@@ -16,360 +16,101 @@ import java.io.FileReader;
 public class Driver {
     public static void main (String[] args)
     {
+        //instance variables
         Array array = new Array();
         Backtrack backtrack;
+        Node[][] originalPuzzle;
         Node[][] solvedPuzzle;
         Object[] colors;
-        //array list of characters
-        ArrayList<ArrayList<Character>> puzzle = new ArrayList<>();
         long startTime;
         long elapsedTime;
+        ArrayList<ArrayList<Character>> puzzle = new ArrayList<>();
 
-        try
+        /* USER INPUT SECTION */
+        boolean error = true;
+        System.out.println("\nWelcome to Flow Solver. Please enter a file name.");
+        System.out.print("Name of Puzzle > ");
+        Scanner in = new Scanner(System.in);
+        String fileName = "";
+
+        //loop until the user enters a valid file name
+        while (error)
         {
-            //opens the file
-            Scanner fileRead = new Scanner(new FileReader("5x5maze.txt"));
-
-            //counter to represent the inner array list
-            int i = 0;
-
-            //loop through the whole file line-by-line
-            while (fileRead.hasNextLine())
+            try
             {
-                //read the next line and convert the entire line to a char array
-                String line = fileRead.nextLine();
-                char[] charLine = line.toCharArray();
-                puzzle.add(new ArrayList<>()); //add an array list inside maze
+                //get the name of the file as the whole line entered
+                fileName = in.nextLine();
 
-                //get the ith element, which is the second layer of the arrayList, and add it to the inner array list
-                for (int j = 0; j < charLine.length; j++)
+                //only accept valid puzzles
+                if (fileName.equals("5x5maze.txt") || fileName.equals("7x7maze.txt") || fileName.equals("8x8maze.txt")
+                        || fileName.equals("9x9maze.txt") || fileName.equals("10x10maze.txt") || fileName.equals("12x12maze.txt"))
                 {
-                    puzzle.get(i).add(charLine[j]);
+                    System.out.println("\n" + fileName + " is valid. Please wait while your puzzle is being solved.");
+
+                    //open the file
+                    Scanner fileRead = new Scanner(new FileReader(fileName));
+
+                    //counter to represent the inner array list
+                    int i = 0;
+
+                    //loop through the whole file line-by-line
+                    while (fileRead.hasNextLine())
+                    {
+                        //read the next line and convert the entire line to a char array
+                        String line = fileRead.nextLine();
+                        char[] charLine = line.toCharArray();
+                        puzzle.add(new ArrayList<>()); //add an array list inside maze
+
+                        //get the ith element, which is the second layer of the arrayList, and add it to the inner array list
+                        for (int j = 0; j < charLine.length; j++)
+                        {
+                            puzzle.get(i).add(charLine[j]);
+                        }
+                        i++; //increment i to the next inner arrayList
+                    }
+                    fileRead.close();
+
+                    //valid input entered
+                    error = false;
                 }
-                i++; //increment i to the next inner arrayList
+
+                //invalid input entered
+                else
+                {
+                    System.out.println("\nInvalid puzzle name. Please try some form of.. NxNmaze.txt");
+                    System.out.print("Name of Puzzle > ");
+                }
             }
-
-            fileRead.close();
-        }
-
-        //error is thrown if file cannot be found
-        catch (Exception exception)
-        {
-            System.out.println(exception);
-        }
-
-        // 5x5 solver
-        Node[][] five = array.convert(puzzle);
-        System.out.println("\n5x5 Puzzle");
-        array.print(five);
-        colors = array.getColors(five);
-        backtrack = new Backtrack(five, colors);
-        startTime = System.nanoTime();
-        solvedPuzzle =  backtrack.dumbSolve();
-        elapsedTime = (System.nanoTime()-startTime)/1000000;
-        System.out.println("\nSolved 5x5 using dumbSolve in " + elapsedTime + " milliseconds, " + backtrack.getDumbCount() + " placements attempted");
-        array.print(solvedPuzzle);
-
-        System.out.println();
-
-        five = array.convert(puzzle);
-        backtrack = new Backtrack(five, colors);
-        startTime = System.nanoTime();
-        solvedPuzzle =  backtrack.smartSolve();
-        elapsedTime = (System.nanoTime()-startTime)/1000000;
-        System.out.println("\nSolved 5x5 using smartSolve in " + elapsedTime + " milliseconds, " + backtrack.getSmartCount() + " placements attempted");
-        array.print(solvedPuzzle);
-
-        //////////////////////////////////////////////////////////////////
-
-        puzzle = new ArrayList<>();
-        try
-        {
-            //opens the file
-            Scanner fileRead = new Scanner(new FileReader("7x7maze.txt"));
-
-            //counter to represent the inner array list
-            int i = 0;
-
-            //loop through the whole file line-by-line
-            while (fileRead.hasNextLine())
+            catch (Exception e)
             {
-                //read the next line and convert the entire line to a char array
-                String line = fileRead.nextLine();
-                char[] charLine = line.toCharArray();
-                puzzle.add(new ArrayList<>()); //add an array list inside maze
-
-                //get the ith element, which is the second layer of the arrayList, and add it to the inner array list
-                for (int j = 0; j < charLine.length; j++)
-                {
-                    puzzle.get(i).add(charLine[j]);
-                }
-                i++; //increment i to the next inner arrayList
+                System.out.println("\nInvalid puzzle name. Please try some form of.. NxNmaze.txt");
+                System.out.print("Name of Puzzle > ");
+                in.next();
             }
-
-            fileRead.close();
         }
 
-        //error is thrown if file cannot be found
-        catch (Exception exception)
-        {
-            System.out.println(exception);
-        }
-
-        // 7x7 solver
-        Node[][] seven = array.convert(puzzle);
-        System.out.println("\n5x5 Puzzle");
-        array.print(seven);
-        colors = array.getColors(seven);
-        backtrack = new Backtrack(seven, colors);
+        //setting the initial conditions; converting, printing, getting domain, starting timer
+        originalPuzzle = array.convert(puzzle);
+        System.out.println("\nOriginal Puzzle");
+        array.print(originalPuzzle);
+        colors = array.getColors(originalPuzzle);
+        backtrack = new Backtrack(originalPuzzle, colors);
         startTime = System.nanoTime();
-        solvedPuzzle =  backtrack.dumbSolve();
+
+        //solve the puzzle using simple backtracking and print out the solution
+        solvedPuzzle = backtrack.dumbSolve();
         elapsedTime = (System.nanoTime()-startTime)/1000000;
-        System.out.println("\nSolved 7x7 using dumbSolve in " + elapsedTime + " milliseconds, " + backtrack.getDumbCount() + " placements attempted");
+        System.out.println("\nSolved " + fileName + " using dumbSolve in " + elapsedTime + " milliseconds, " + backtrack.getDumbCount() + " placements attempted");
         array.print(solvedPuzzle);
 
-        System.out.println();
-
-        seven = array.convert(puzzle);
-        backtrack = new Backtrack(seven, colors);
+        //solve the same puzzle using advanced backtracking and print out the solution
+        originalPuzzle = array.convert(puzzle);
+        backtrack = new Backtrack(originalPuzzle, colors);
         startTime = System.nanoTime();
-        solvedPuzzle =  backtrack.smartSolve();
+        solvedPuzzle = backtrack.smartSolve();
         elapsedTime = (System.nanoTime()-startTime)/1000000;
-        System.out.println("\nSolved 7x7 using smartSolve in " + elapsedTime + " milliseconds, " + backtrack.getSmartCount() + " placements attempted");
+        System.out.println("\nSolved " + fileName + " using smartSolve in " + elapsedTime + " milliseconds, " + backtrack.getSmartCount() + " placements attempted");
         array.print(solvedPuzzle);
-
-        /////////////////////////////////////////////////////////////////
-
-        puzzle = new ArrayList<>();
-        try
-        {
-            //opens the file
-            Scanner fileRead = new Scanner(new FileReader("8x8maze.txt"));
-
-            //counter to represent the inner array list
-            int i = 0;
-
-            //loop through the whole file line-by-line
-            while (fileRead.hasNextLine())
-            {
-                //read the next line and convert the entire line to a char array
-                String line = fileRead.nextLine();
-                char[] charLine = line.toCharArray();
-                puzzle.add(new ArrayList<>()); //add an array list inside maze
-
-                //get the ith element, which is the second layer of the arrayList, and add it to the inner array list
-                for (int j = 0; j < charLine.length; j++)
-                {
-                    puzzle.get(i).add(charLine[j]);
-                }
-                i++; //increment i to the next inner arrayList
-            }
-
-            fileRead.close();
-        }
-
-        //error is thrown if file cannot be found
-        catch (Exception exception)
-        {
-            System.out.println(exception);
-        }
-
-        // 8x8 solver
-        Node[][] eight = array.convert(puzzle);
-        System.out.println("\n8x8 Puzzle");
-        array.print(eight);
-        colors = array.getColors(eight);
-        backtrack = new Backtrack(eight, colors);
-        startTime = System.nanoTime();
-        solvedPuzzle =  backtrack.dumbSolve();
-        elapsedTime = (System.nanoTime()-startTime)/1000000;
-        System.out.println("\nSolved 8x8 using dumbSolve in " + elapsedTime + " milliseconds, " + backtrack.getDumbCount() + " placements attempted");
-        array.print(solvedPuzzle);
-
-        System.out.println();
-
-        eight = array.convert(puzzle);
-        backtrack = new Backtrack(eight, colors);
-        startTime = System.nanoTime();
-        solvedPuzzle =  backtrack.smartSolve();
-        elapsedTime = (System.nanoTime()-startTime)/1000000;
-        System.out.println("\nSolved 8x8 using smartSolve in " + elapsedTime + " milliseconds, " + backtrack.getSmartCount() + " placements attempted");
-        array.print(solvedPuzzle);
-
-        ///////////////////////////////////////////////////////////////////
-
-        puzzle = new ArrayList<>();
-        try
-        {
-            //opens the file
-            Scanner fileRead = new Scanner(new FileReader("9x9maze.txt"));
-
-            //counter to represent the inner array list
-            int i = 0;
-
-            //loop through the whole file line-by-line
-            while (fileRead.hasNextLine())
-            {
-                //read the next line and convert the entire line to a char array
-                String line = fileRead.nextLine();
-                char[] charLine = line.toCharArray();
-                puzzle.add(new ArrayList<>()); //add an array list inside maze
-
-                //get the ith element, which is the second layer of the arrayList, and add it to the inner array list
-                for (int j = 0; j < charLine.length; j++)
-                {
-                    puzzle.get(i).add(charLine[j]);
-                }
-                i++; //increment i to the next inner arrayList
-            }
-
-            fileRead.close();
-        }
-
-        //error is thrown if file cannot be found
-        catch (Exception exception)
-        {
-            System.out.println(exception);
-        }
-
-        // 9x9 solver
-        Node[][] nine = array.convert(puzzle);
-        System.out.println("\n9x9 Puzzle");
-        array.print(nine);
-        colors = array.getColors(nine);
-        backtrack = new Backtrack(nine, colors);
-        startTime = System.nanoTime();
-        solvedPuzzle =  backtrack.dumbSolve();
-        elapsedTime = (System.nanoTime()-startTime)/1000000;
-        System.out.println("\nSolved 9x9 using dumbSolve in " + elapsedTime + " milliseconds, " + backtrack.getDumbCount() + " placements attempted");
-        array.print(solvedPuzzle);
-
-        System.out.println();
-
-        nine = array.convert(puzzle);
-        backtrack = new Backtrack(nine, colors);
-        startTime = System.nanoTime();
-        solvedPuzzle =  backtrack.smartSolve();
-        elapsedTime = (System.nanoTime()-startTime)/1000000;
-        System.out.println("\nSolved 9x9 using smartSolve in " + elapsedTime + " milliseconds, " + backtrack.getSmartCount() + " placements attempted");
-        array.print(solvedPuzzle);
-
-        ////////////////////////////////////////////////////////////////////
-
-        puzzle = new ArrayList<>();
-        try
-        {
-            //opens the file
-            Scanner fileRead = new Scanner(new FileReader("10x10maze.txt"));
-
-            //counter to represent the inner array list
-            int i = 0;
-
-            //loop through the whole file line-by-line
-            while (fileRead.hasNextLine())
-            {
-                //read the next line and convert the entire line to a char array
-                String line = fileRead.nextLine();
-                char[] charLine = line.toCharArray();
-                puzzle.add(new ArrayList<>()); //add an array list inside maze
-
-                //get the ith element, which is the second layer of the arrayList, and add it to the inner array list
-                for (int j = 0; j < charLine.length; j++)
-                {
-                    puzzle.get(i).add(charLine[j]);
-                }
-                i++; //increment i to the next inner arrayList
-            }
-
-            fileRead.close();
-        }
-
-        //error is thrown if file cannot be found
-        catch (Exception exception)
-        {
-            System.out.println(exception);
-        }
-
-        // 10x10 solver
-        Node[][] ten = array.convert(puzzle);
-        System.out.println("\n10x10 Puzzle");
-        array.print(ten);
-        colors = array.getColors(ten);
-        backtrack = new Backtrack(ten, colors);
-        startTime = System.nanoTime();
-        solvedPuzzle =  backtrack.dumbSolve();
-        elapsedTime = (System.nanoTime()-startTime)/1000000;
-        System.out.println("\nSolved 10x10 using dumbSolve in " + elapsedTime + " milliseconds, " + backtrack.getDumbCount() + " placements attempted");
-        array.print(solvedPuzzle);
-
-        System.out.println();
-
-        ten = array.convert(puzzle);
-        backtrack = new Backtrack(ten, colors);
-        startTime = System.nanoTime();
-        solvedPuzzle =  backtrack.smartSolve();
-        elapsedTime = (System.nanoTime()-startTime)/1000000;
-        System.out.println("\nSolved 10x10 using smartSolve in " + elapsedTime + " milliseconds, " + backtrack.getSmartCount() + " placements attempted");
-        array.print(solvedPuzzle);
-
-        //////////////////////////////////////////////////////////////////
-
-        /*puzzle = new ArrayList<>();
-        try
-        {
-            //opens the file
-            Scanner fileRead = new Scanner(new FileReader("12x12maze.txt"));
-
-            //counter to represent the inner array list
-            int i = 0;
-
-            //loop through the whole file line-by-line
-            while (fileRead.hasNextLine())
-            {
-                //read the next line and convert the entire line to a char array
-                String line = fileRead.nextLine();
-                char[] charLine = line.toCharArray();
-                puzzle.add(new ArrayList<>()); //add an array list inside maze
-
-                //get the ith element, which is the second layer of the arrayList, and add it to the inner array list
-                for (int j = 0; j < charLine.length; j++)
-                {
-                    puzzle.get(i).add(charLine[j]);
-                }
-                i++; //increment i to the next inner arrayList
-            }
-
-            fileRead.close();
-        }
-
-        //error is thrown if file cannot be found
-        catch (Exception exception)
-        {
-            System.out.println(exception);
-        }
-
-        // 12x12 solver
-        Node[][] twelve = array.convert(puzzle);
-        System.out.println("\n12x12 Puzzle");
-        array.print(twelve);
-        colors = array.getColors(twelve);
-        backtrack = new Backtrack(twelve, colors);
-        startTime = System.nanoTime();
-        solvedPuzzle =  backtrack.dumbSolve();
-        elapsedTime = (System.nanoTime()-startTime)/1000000;
-        System.out.println("\nSolved 12x12 using dumbSolve in " + elapsedTime + " milliseconds, " + backtrack.getDumbCount() + " placements attempted");
-        array.print(solvedPuzzle);
-
-        System.out.println();
-
-        twelve = array.convert(puzzle);
-        backtrack = new Backtrack(twelve, colors);
-        startTime = System.nanoTime();
-        solvedPuzzle =  backtrack.smartSolve();
-        elapsedTime = (System.nanoTime()-startTime)/1000000;
-        System.out.println("\nSolved 12x12 using smartSolve in " + elapsedTime + " milliseconds, " + backtrack.getSmartCount() + " placements attempted");
-        array.print(solvedPuzzle);*/
-
 
 
         /* TESTING */
